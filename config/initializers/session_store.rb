b4045336d7af721f8d7c909b35a731430f63fbe2
twothicc <chenenliuu@gmail.com>
2020-12-28 01:18:53 +0800

    changed session_store env to production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
-if Rails.env === 'development' 
+if Rails.env === 'production' 
     Rails.application.config.session_store :cookie_store, key: '_todo_app', domain: ''
 else
     Rails.application.config.session_store :cookie_store, key: '_todo_app'
